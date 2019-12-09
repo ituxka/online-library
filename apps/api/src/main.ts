603349296997bff94d50c56ae674000f7a2db043
nikita.solohub <ituxka@gmail.com>
2019-12-09 13:07:04 +0300

    refactor: get port from environment object

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,14 +6,17 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
+
+  const port = environment.apiPort;
+
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    console.log(`Listening at http://localhost:${port}/${globalPrefix}`);
   });
 }
 
