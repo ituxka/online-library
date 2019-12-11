85ad75f70d4158dff3359538eb9b462b5b351823
nikita.solohub <ituxka@gmail.com>
2019-12-11 12:18:24 +0300

    refactor: add better subscription handling

--- apps/online-library/src/app/modules/auth/signin/containers/signin/signin.component.ts
+++ apps/online-library/src/app/modules/auth/signin/containers/signin/signin.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthStoreService } from '../../../state';
 import { SnackbarService } from '../../../../shared/services/snackbar/snackbar.service';
 import { Router } from '@angular/router';
+import { untilDestroyed } from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'ol-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent implements OnInit, OnDestroy {
   signInForm: FormGroup;
 
   hidePassword = true;
@@ -23,6 +24,10 @@ export class SigninComponent implements OnInit {
 
   ngOnInit() {
     this.initForm();
+  }
+
+  // must be present for untilDestroy operator
+  ngOnDestroy() {
   }
 
   initForm() {
@@ -41,13 +46,15 @@ export class SigninComponent implements OnInit {
     }
 
     const { email, password } = this.signInForm.value;
-    this.authStoreService.signIn(email, password).subscribe({
-      next: () => {
-        this.snackBar.openSuccess('Signin successful');
-        this.router.navigate(['/']).then();
-      },
-      error: () => this.snackBar.openError('Invalid credentials'),
-    });
+    this.authStoreService.signIn(email, password)
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: () => {
+          this.snackBar.openSuccess('Signin successful');
+          this.router.navigate(['/']).then();
+        },
+        error: () => this.snackBar.openError('Invalid credentials'),
+      });
   }
 
   onClear() {
--- apps/online-library/src/app/modules/auth/signup/containers/singup/signup.component.ts
+++ apps/online-library/src/app/modules/auth/signup/containers/singup/signup.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthStoreService } from '../../../state';
 import { SnackbarService } from '../../../../shared/services/snackbar/snackbar.service';
 import { Router } from '@angular/router';
+import { untilDestroyed } from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'ol-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   signUpForm: FormGroup;
 
   hidePassword = true;
@@ -23,6 +24,10 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
     this.initForm();
+  }
+
+  // must be present for untilDestroy operator
+  ngOnDestroy() {
   }
 
   initForm() {
@@ -41,13 +46,15 @@ export class SignupComponent implements OnInit {
     }
 
     const { email, password } = this.signUpForm.value;
-    this.authStoreService.signUp(email, password).subscribe({
-      next: () => {
-        this.snackBar.openSuccess('Register successful');
-        this.router.navigate(['/']).then();
-      },
-      error: ({ error }) => this.snackBar.openError(error.message),
-    });
+    this.authStoreService.signUp(email, password)
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: () => {
+          this.snackBar.openSuccess('Register successful');
+          this.router.navigate(['/']).then();
+        },
+        error: ({ error }) => this.snackBar.openError(error.message),
+      });
   }
 
   onClear() {
