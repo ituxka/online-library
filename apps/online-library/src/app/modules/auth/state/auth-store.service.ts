7362d0dfd0fb30b7a8564480c74996a07b2256bf
nikita.solohub <ituxka@gmail.com>
2019-12-11 12:23:06 +0300

    refactor: add interfaces to signin/signup, shareable with backend

--- apps/online-library/src/app/modules/auth/state/auth-store.service.ts
+++ apps/online-library/src/app/modules/auth/state/auth-store.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AuthStore } from './auth.store';
+import { AuthStore, createInitialState } from './auth.store';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { pipe } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { AuthResult } from '@online-library/api-interfaces';
-import {
-  SignInUserDTO,
-  SignUpUserDTO,
-} from '../../../../../../api/src/app/modules/user/dtos/create-user.dto';
+import { AuthResult, SignInPayload, SignUpPayload } from '@online-library/api-interfaces';
 import { setLoading } from '@datorama/akita';
 
 @Injectable()
@@ -31,7 +27,7 @@ export class AuthStoreService {
   signUp(email: string, password: string) {
     return this.http
       // TODO need better url building
-      .post<AuthResult>(`${this.url}auth/signup`, { email, password } as SignUpUserDTO)
+      .post<AuthResult>(`${this.url}auth/signup`, { email, password } as SignUpPayload)
       .pipe(
         setLoading(this.authStore),
         this.updateStore$,
@@ -40,7 +36,7 @@ export class AuthStoreService {
 
   signIn(email: string, password: string) {
     return this.http
-      .post<AuthResult>(`${this.url}auth/signin`, { email, password } as SignInUserDTO)
+      .post<AuthResult>(`${this.url}auth/signin`, { email, password } as SignInPayload)
       .pipe(
         setLoading(this.authStore),
         this.updateStore$,
