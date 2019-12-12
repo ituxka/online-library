46db469871420e2713f3aa09da43403a907ab484
nikita.solohub <ituxka@gmail.com>
2019-12-12 15:59:45 +0300

    refactor: add type for typechecking

--- apps/api/src/app/modules/user/user.providers.ts
+++ apps/api/src/app/modules/user/user.providers.ts
@@ -2,8 +2,9 @@ import { USER_REPOSITORY } from './user.constants';
 import { Connection } from 'typeorm';
 import { User } from './user.entity';
 import { DATABASE_CONNECTION } from '../../database/constants';
+import { Provider } from '@nestjs/common';
 
-export const userProviders = [
+export const userProviders: Provider[] = [
   {
     provide: USER_REPOSITORY,
     useFactory: (connection: Connection) => connection.getRepository(User),
