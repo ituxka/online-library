98b05b9382a481e3e77449251056ff9befb0ffbc
nikita.solohub <ituxka@gmail.com>
2019-12-18 13:19:48 +0300

    refactor: use already existing method to check book availability to order

--- apps/api/src/app/modules/book/book.service.ts
+++ apps/api/src/app/modules/book/book.service.ts
@@ -3,17 +3,20 @@ import { Book } from './book.entity';
 import { Inject, Injectable } from '@nestjs/common';
 import { BOOK_REPOSITORY } from './book.constants';
 import { Repository } from 'typeorm';
+import { IBook } from '@online-library/api-interfaces';
+import { BookingService } from '../booking/booking.service';
 
 @Injectable()
 export class BookService extends TypeOrmCrudService<Book>{
   constructor(
     @Inject(BOOK_REPOSITORY) protected bookRepository: Repository<Book>,
+    private bookingService: BookingService,
   ) {
     super(bookRepository);
   }
 
-  async updateAvailability(book: Book) {
-    const isAvailable = book.copiesInUse + book.copiesBooked < book.copies;
+  async updateAvailability(book: IBook) {
+    const isAvailable = this.bookingService.checkAvailability(book);
     const bookFromDB = await this.bookRepository.findOne({ id: book.id });
     bookFromDB.isAvailableToBook = isAvailable;
     return this.bookRepository.save(bookFromDB);
