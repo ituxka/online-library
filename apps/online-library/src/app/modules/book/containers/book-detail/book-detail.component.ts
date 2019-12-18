debee6ab1823151f446ba3caed617f6ece0b9bde
nikita.solohub <ituxka@gmail.com>
2019-12-18 16:47:10 +0300

    feat: update view on order action

--- apps/online-library/src/app/modules/book/containers/book-detail/book-detail.component.ts
+++ apps/online-library/src/app/modules/book/containers/book-detail/book-detail.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs/operators';
 import { BookService } from '../../book.service';
 import { IBook, UserRole } from '@online-library/api-interfaces';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { BookingService } from '../../booking.service';
 import { AuthQuery, AuthStoreService } from '../../../auth/state';
 import { SnackbarService } from '../../../shared/services/snackbar/snackbar.service';
@@ -17,13 +17,22 @@ import { untilDestroyed } from 'ngx-take-until-destroy';
 export class BookDetailComponent implements OnDestroy {
   roles = UserRole;
 
-  book$: Observable<IBook> = this.route.paramMap
+  updateBook$ = new BehaviorSubject(null);
+
+  book$: Observable<IBook> = combineLatest(this.route.paramMap, this.updateBook$)
     .pipe(
+      map(([paramMap]) => paramMap),
       map(paramMap => paramMap.get('id')),
       map(idAsString => parseInt(idAsString, 10)),
       filter(parsed => !isNaN(parsed)),
       switchMap(id => this.bookService.getById(id)),
     );
+
+  isAvailableToOrder$ = this.book$.pipe(
+    map(book =>
+      book.holders.find(holder => holder.id !== this.authQuery.userId()),
+    ),
+  );
 
   imagePlaceholder = 'https://via.placeholder.com/1000';
 
@@ -48,7 +57,8 @@ export class BookDetailComponent implements OnDestroy {
       .subscribe({
         next: (user) => {
           this.authStoreService.setUser(user);
-          this.snackbarService.openSuccess('Successfully booked!');
+          this.updateBook$.next(null);
+          this.snackbarService.openSuccess('Successfully ordered!');
         },
         error: ({ error }) => this.snackbarService.openError(error.message),
       });
